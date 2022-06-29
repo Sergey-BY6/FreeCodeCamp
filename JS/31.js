01f6f54387bcef1cd132b20bb67ef1d9a7947f85
Sergey Babich <sergei.babich2022@yandex.by>
2022-06-30 00:31:20 +0300

    add 31-50 js file FreeCodeCamp

---
+++ JS/31.js
@@ -0,0 +1,4 @@
+// Change code below this line
+const someAdjective = "123";
+let myStr = "Learning to code is ";
+myStr += someAdjective
---
+++ JS/48.js
@@ -0,0 +1,2 @@
+const myList = [ ["Chocolate Bar", 15], ["Milk chocolate Bar", 16],
+["Dark chocolate Bar", 17], ["Fanta", 18], ["Coca-Cola", 19] ];
