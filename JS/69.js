4feacfebfedd782ec2ebacfd8c7e909a7cae296d
Sergey Babich <sergei.babich2022@yandex.by>
2022-07-01 01:28:39 +0300

    add 67-71 js file FreeCodeCamp

---
+++ JS/69.js
@@ -0,0 +1,9 @@
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <=50 && val >=25)  return "Yes";
+
+else return "No";
+}
+
+testLogicalAnd(10);
---
+++ JS/70.js
@@ -0,0 +1,8 @@
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val <10 || val >20 )return "Outside";
+else return "Inside";
+}
+
+testLogicalOr(15);
